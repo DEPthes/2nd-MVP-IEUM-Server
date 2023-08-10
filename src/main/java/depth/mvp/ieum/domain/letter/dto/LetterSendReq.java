af19efd0b5a2fcdfb493f480d64f80ee5bb4f5d0
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2023-08-10 22:03:05 +0900

    [REFACTOR]: 제목 최대 28자, 내용 최대 3800자 제한 추가

--- src/main/java/depth/mvp/ieum/domain/letter/dto/LetterSendReq.java
+++ src/main/java/depth/mvp/ieum/domain/letter/dto/LetterSendReq.java
@@ -2,6 +2,7 @@ package depth.mvp.ieum.domain.letter.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,11 @@ import lombok.NoArgsConstructor;
 public class LetterSendReq {
 
     @NotBlank(message = "제목을 입력해야 합니다.")
+    @Size(max = 28, message = "제목은 최대 28자까지 입력 가능합니다.")
     private String title;
 
     @NotBlank(message = "내용을 입력해야 합니다.")
+    @Size(max = 3500, message = "내용은 최대 3500자까지 입력 가능합니다.")
     @Pattern(regexp = "^(?!.*<img).*", message = "이미지 삽입은 허용되지 않습니다.")
     private String contents;
 
