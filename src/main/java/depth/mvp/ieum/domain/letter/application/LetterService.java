4d12eb2285ab013628ca08836f29d740bcbccede
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2023-08-13 23:28:54 +0900

    [REFACTOR]: 코드 리팩토링(편지 작성, 답장 기능 통합)

--- src/main/java/depth/mvp/ieum/domain/letter/application/LetterService.java
+++ src/main/java/depth/mvp/ieum/domain/letter/application/LetterService.java
@@ -3,11 +3,12 @@ package depth.mvp.ieum.domain.letter.application;
 import depth.mvp.ieum.domain.letter.domain.Letter;
 import depth.mvp.ieum.domain.letter.domain.LetterType;
 import depth.mvp.ieum.domain.letter.domain.repository.LetterRepository;
-import depth.mvp.ieum.domain.letter.dto.LetterSendReq;
+import depth.mvp.ieum.domain.letter.dto.LetterReq;
 import depth.mvp.ieum.domain.mail.MailService;
 import depth.mvp.ieum.domain.user.domain.User;
 import depth.mvp.ieum.domain.user.domain.repository.UserRepository;
 import depth.mvp.ieum.global.config.security.token.UserPrincipal;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,7 @@ import java.util.Random;
 
 @RequiredArgsConstructor
 @Service
-public class LetterSendService {
+public class LetterService {
 
     private final LetterRepository letterRepository;
     private final UserRepository userRepository;
@@ -25,12 +26,27 @@ public class LetterSendService {
 
     // 편지 작성
     @Transactional
-    public Letter writeLetter(UserPrincipal userPrincipal, LetterSendReq letterReq) {
+    public Letter writeLetter(UserPrincipal userPrincipal, LetterReq letterReq) {
+
         User user = userRepository.findById(userPrincipal.getId())
                 .orElseThrow();
-        User receiver = getRandomReceiver(user);
 
-        Letter letter = Letter.builder()
+        Letter letter;
+        User receiver;
+
+        if (letterReq.getOriginalLetterId() == null) {
+            // 편지 신규 작성
+            receiver = getRandomReceiver(user);
+        } else {
+            // 편지 답장
+            Letter originalLetter = getOriginalLetterAndCheckReceiver(letterReq.getOriginalLetterId(), user);
+
+            // 원본 편지 정보에서 발신인 id를 찾아 수신인으로 설정
+            receiver = userRepository.findById(originalLetter.getSender().getId())
+                    .orElseThrow(() -> new EntityNotFoundException("수신인을 찾을 수 없습니다."));
+        }
+
+        letter = Letter.builder()
                 .sender(user)
                 .receiver(receiver)
                 .title(letterReq.getTitle())
@@ -41,23 +57,35 @@ public class LetterSendService {
                 .build();
 
         letterRepository.save(letter);
+
         // 수신인에게 메일 발송
         sendEmailToReceiver(receiver.getEmail());
-
         return letter;
     }
 
-    // 메일 전송 메소드
+    // 메일 발송
     protected void sendEmailToReceiver(String email) {
         mailService.sendEmailToReceiver(email);
     }
 
-    // 편지 발송 시 수신인 랜덤 지정
+    // (퍈지 신규 작성) 편지 발송 시 수신인 랜덤 지정
     private User getRandomReceiver(User sender) {
         List<User> receivers = userRepository.findByIdNot(sender.getId());
 
         Random random = new Random();
         return receivers.get(random.nextInt(receivers.size()));
+    }
+
+    // (편지 답장) 원본 편지의 수신인과 현재 사용자가 동일한지 확인
+    private Letter getOriginalLetterAndCheckReceiver(Long originalLetterId, User user) {
+        Letter originalLetter = letterRepository.findById(originalLetterId)
+                .orElseThrow(() -> new EntityNotFoundException("원본 편지를 찾을 수 없습니다."));
+
+        if (!originalLetter.getReceiver().getId().equals(user.getId())) {
+            throw new IllegalArgumentException("원본 편지의 수신자와 현재 사용자가 다릅니다.");
+        }
+
+        return originalLetter;
     }
 
 
--- src/main/java/depth/mvp/ieum/domain/letter/dto/LetterReq.java
+++ src/main/java/depth/mvp/ieum/domain/letter/dto/LetterReq.java
@@ -10,7 +10,9 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class LetterSendReq {
+public class LetterReq {
+
+    private Long originalLetterId; // (편지 답장 시) 답장할 편지의 id
 
     @NotBlank(message = "제목을 입력해야 합니다.")
     @Size(max = 28, message = "제목은 최대 28자까지 입력 가능합니다.")
@@ -21,5 +23,5 @@ public class LetterSendReq {
     @Pattern(regexp = "^(?!.*<img).*", message = "이미지 삽입은 허용되지 않습니다.")
     private String contents;
 
-    private int envelopType;
+    private Integer envelopType;
 }
