5f60d8c3a58d0dcbd2f6b1e74714bcdc5b811845
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2023-08-13 23:41:30 +0900

    [REFACTOR]: 편지 조회 기능 중 중복되는 코드 메소드로 분리

--- src/main/java/depth/mvp/ieum/domain/letter/application/MailBoxService.java
+++ src/main/java/depth/mvp/ieum/domain/letter/application/MailBoxService.java
@@ -21,54 +21,24 @@ public class MailBoxService {
 
     private final LetterRepository letterRepository;
 
-    // 우체통 - 받은 편지 모아보기(제목, 수신날짜, 발신자 닉네임)
+    // 우체통 - 받은 편지 리스트로 조회
     // 안 읽은 편지
     public List<MailBoxRes> getUnreadLetters(Long userId) {
-
-        List<MailBoxRes> unreadMailBoxes = new ArrayList<>();
         List<Letter> unreadLetters = letterRepository.findByReceiver_IdAndIsReadAndLetterType(userId, false, LetterType.SENT);
-
-        for (Letter letter : unreadLetters) {
-            String senderNickname = letter.getSender().getNickname();
-
-            MailBoxRes mailBoxRes = MailBoxRes.builder()
-                    .letterId(letter.getId())
-                    .senderNickname(senderNickname)
-                    .title(letter.getTitle())
-                    .modifiedAt(letter.getModifiedAt())
-                    .build();
-            unreadMailBoxes.add(mailBoxRes);
-        }
-        return sortByLatestDate(unreadMailBoxes);
+        return lettersToMailBoxResList(unreadLetters);
     }
 
+    // 읽은 편지
     public List<MailBoxRes> getReadLetters(Long userId) {
-
-        List<MailBoxRes> readMailBoxes = new ArrayList<>();
         List<Letter> readLetters = letterRepository.findByReceiver_IdAndIsReadAndLetterType(userId, true, LetterType.SENT);
-
-        for (Letter letter : readLetters) {
-            String senderNickname = letter.getSender().getNickname();
-
-            MailBoxRes mailBoxRes = MailBoxRes.builder()
-                    .letterId(letter.getId())
-                    .senderNickname(senderNickname)
-                    .title(letter.getTitle())
-                    .modifiedAt(letter.getModifiedAt())
-                    .build();
-            readMailBoxes.add(mailBoxRes);
-        }
-        return sortByLatestDate(readMailBoxes);
+        return lettersToMailBoxResList(readLetters);
     }
 
-    // 편지 상세 보기(읽음 여부 true 변경)
-    // 기능 명세 보고 다시 수정
-    // 최신 순서로 정렬
+    // 편지 상세 조회(읽음 여부 true 변경)
     public Letter getLetterDetails(Long userId, Long letterId) {
         Letter letter = letterRepository.findById(letterId)
                 .orElseThrow(() -> new EntityNotFoundException("원본 편지를 찾을 수 없습니다."));
 
-        // originalLetter의 receiver와 user가 동일한지 확인하는 로직
         if (!letter.getReceiver().getId().equals(userId)) {
             throw new IllegalArgumentException("원본 편지의 수신자와 현재 사용자가 다릅니다.");
         }
@@ -79,6 +49,24 @@ public class MailBoxService {
         return letter;
     }
 
+    private List<MailBoxRes> lettersToMailBoxResList(List<Letter> letters) {
+        List<MailBoxRes> mailBoxResList = new ArrayList<>();
+
+        for (Letter letter : letters) {
+            String senderNickname = letter.getSender().getNickname();
+
+            MailBoxRes mailBoxRes = MailBoxRes.builder()
+                    .letterId(letter.getId())
+                    .senderNickname(senderNickname)
+                    .title(letter.getTitle())
+                    .modifiedAt(letter.getModifiedAt())
+                    .build();
+
+            mailBoxResList.add(mailBoxRes);
+        }
+
+        return sortByLatestDate(mailBoxResList);
+    }
 
     public List<MailBoxRes> sortByLatestDate(List<MailBoxRes> mailBoxes) {
         if (mailBoxes == null || mailBoxes.isEmpty()) {
--- src/main/java/depth/mvp/ieum/domain/letter/dto/MailBoxDetailsRes.java
+++ src/main/java/depth/mvp/ieum/domain/letter/dto/MailBoxDetailsRes.java
@@ -19,4 +19,5 @@ public class MailBoxDetailsRes {
 
     private boolean isRead;
 
+
 }
